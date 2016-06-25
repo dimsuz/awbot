8e9c31aa3d63df39ade7d53c46603259dfffd5b0
Dmitry Suzdalev <dz@appkode.ru>
2016-06-25 22:39:45 +0200

    Add a test getUpdates call

--- Handler/Bot.hs
+++ Handler/Bot.hs
@@ -1,9 +1,20 @@
 module Handler.Bot where
 
 import Import
+import Network.HTTP.Simple
+import Data.Aeson as Json
 
-token :: Text
+token :: String
 token = "200098948:AAE-bwgutgijahNPYpmQbKBARI7Rh-wFAbM"
 
+apiCall :: (MonadThrow m, MonadIO m, FromJSON a) => String -> m (Response a)
+apiCall methodName = do
+  request <- parseUrl ("https://api.telegram.org/bot" `mappend` token `mappend` "/" `mappend` methodName)
+  httpJSON request
+
 getBotRefreshR :: Handler ()
-getBotRefreshR = sendResponse ()
+getBotRefreshR = do
+  response <- apiCall "getUpdates" :: Handler (Response Value)
+  putStrLn $ "The status code was: " ++
+               (pack . show) (getResponseStatusCode response)
+  print $ Json.encode (getResponseBody response)
