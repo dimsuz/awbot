eb9474e675d5c2e726d0081d559993f8e15fda86
Dmitry Suzdalev <dz@appkode.ru>
2016-06-26 13:42:13 +0200

    Implement a basic parsing of getUpdates json

--- Handler/Bot.hs
+++ Handler/Bot.hs
@@ -1,20 +1,46 @@
+{-# LANGUAGE DeriveGeneric #-}
 module Handler.Bot where
 
-import Import
+import Import hiding (Update)
 import Network.HTTP.Simple
 import Data.Aeson as Json
+import Data.Aeson.Types (Parser, parseEither)
+import qualified Data.ByteString.Lazy as B
+
+data Message = Message {
+    message_id :: Int
+ -- , from
+    , text :: Maybe Text
+  } deriving (Generic, Show)
+
+data Update = Update {
+    update_id :: Int
+  , message :: Maybe Message
+  , edited_message :: Maybe Message
+  } deriving (Generic, Show)
+
+instance FromJSON Update
+instance FromJSON Message
+
+unwrap :: FromJSON a => String -> Value -> Parser [a]
+unwrap name = withObject name $ \o -> o .: "result"
 
 token :: String
 token = "200098948:AAE-bwgutgijahNPYpmQbKBARI7Rh-wFAbM"
 
-apiCall :: (MonadThrow m, MonadIO m, FromJSON a) => String -> m (Response a)
+apiCall :: (MonadThrow m, MonadIO m) => String -> m (Response B.ByteString)
 apiCall methodName = do
   request <- parseUrl ("https://api.telegram.org/bot" `mappend` token `mappend` "/" `mappend` methodName)
-  httpJSON request
+  httpLBS request
+
+decodeUpdates :: B.ByteString -> Either String [Update]
+decodeUpdates s = do
+  decoded <- Json.eitherDecode s
+  parseEither (unwrap "updates") decoded
 
 getBotRefreshR :: Handler ()
 getBotRefreshR = do
-  response <- apiCall "getUpdates" :: Handler (Response Value)
+  response <- apiCall "getUpdates"
   putStrLn $ "The status code was: " ++
                (pack . show) (getResponseStatusCode response)
-  print $ Json.encode (getResponseBody response)
+  print $ decodeUpdates (getResponseBody response)
